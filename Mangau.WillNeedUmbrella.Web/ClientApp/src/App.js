b3d544af77bde5b07bd72f90c3c5dd872b02c11a
Abimael Ordoñez Avila <abimael.avila@mangausistemas.com>
2019-10-19 09:34:21 -0600

    Added NET Core 3.0 React Project from Visual Studio template.
    Converted ClientApp to use TypeScript and removed some boilerplate files.

---
+++ Mangau.WillNeedUmbrella.Web/ClientApp/src/App.js
@@ -0,0 +1,33 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+require("./App.css");
+var logo_svg_1 = require("./logo.svg");
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    App.prototype.render = function () {
+        return (React.createElement("div", { className: "App" },
+            React.createElement("header", { className: "App-header" },
+                React.createElement("img", { src: logo_svg_1.default, className: "App-logo", alt: "logo" }),
+                React.createElement("h1", { className: "App-title" }, "Welcome to Mangau Will Need Umbrella"))));
+    };
+    return App;
+}(React.Component));
+exports.default = App;
+//# sourceMappingURL=App.js.map
