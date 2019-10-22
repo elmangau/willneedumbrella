c68f5a88a1e595a6b4483d0fb029ce226ed9d24b
Abimael Ordoñez Avila <abimael.avila@mangausistemas.com>
2019-10-22 11:43:29 -0600

    Added React Page Router.

--- Mangau.WillNeedUmbrella.Web/ClientApp/src/App.js
+++ Mangau.WillNeedUmbrella.Web/ClientApp/src/App.js
@@ -14,6 +14,7 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
+var react_router_dom_1 = require("react-router-dom");
 require("./App.css");
 var logo_svg_1 = require("./logo.svg");
 var App = /** @class */ (function (_super) {
@@ -22,12 +23,25 @@ var App = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     App.prototype.render = function () {
-        return (React.createElement("div", { className: "App" },
-            React.createElement("header", { className: "App-header" },
-                React.createElement("img", { src: logo_svg_1.default, className: "App-logo", alt: "logo" }),
-                React.createElement("h1", { className: "App-title" }, "Welcome to Mangau Will Need Umbrella"))));
+        return (React.createElement(react_router_dom_1.BrowserRouter, null,
+            React.createElement("div", null,
+                React.createElement("div", { className: "App" },
+                    React.createElement("header", { className: "App-header" },
+                        React.createElement("img", { src: logo_svg_1.default, className: "App-logo", alt: "logo" }),
+                        React.createElement("h1", { className: "App-title" }, "Welcome to Mangau Will Need Umbrella"))),
+                React.createElement("nav", null,
+                    React.createElement(react_router_dom_1.Link, { to: "/" }, "Home"),
+                    React.createElement(react_router_dom_1.Link, { to: "/foo" }, "Foo"),
+                    React.createElement(react_router_dom_1.Link, { to: "/bar" }, "Bar")),
+                React.createElement(react_router_dom_1.Switch, null,
+                    React.createElement(react_router_dom_1.Route, { exact: true, path: "/", component: exports.Home }),
+                    React.createElement(react_router_dom_1.Route, { exact: true, path: "/foo", component: exports.Foo }),
+                    React.createElement(react_router_dom_1.Route, { exact: true, path: "/bar", component: exports.Bar })))));
     };
     return App;
 }(React.Component));
 exports.default = App;
+exports.Home = function () { return React.createElement("h1", null, "Home Page"); };
+exports.Foo = function () { return React.createElement("h1", null, "Foo Page"); };
+exports.Bar = function () { return React.createElement("h1", null, "Bar Page"); };
 //# sourceMappingURL=App.js.map
--- Mangau.WillNeedUmbrella.Web/ClientApp/src/App.tsx
+++ Mangau.WillNeedUmbrella.Web/ClientApp/src/App.tsx
@@ -1,19 +1,37 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
-
 import logo from './logo.svg';
 
 class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to Mangau Will Need Umbrella</h1>
-                </header>
-            </div>
+            <Router>
+                <div>
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h1 className="App-title">Welcome to Mangau Will Need Umbrella</h1>
+                        </header>
+                    </div>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/foo">Foo</Link>
+                        <Link to="/bar">Bar</Link>
+                    </nav>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/foo" component={Foo} />
+                        <Route exact path="/bar" component={Bar} />
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
 
 export default App;
+
+export const Home = () => <h1>Home Page</h1>;
+export const Foo = () => <h1>Foo Page</h1>;
+export const Bar = () => <h1>Bar Page</h1>;
