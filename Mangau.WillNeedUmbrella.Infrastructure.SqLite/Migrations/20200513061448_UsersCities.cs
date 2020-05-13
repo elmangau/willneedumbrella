4c5e5ff3cd9a4e95df0059f888541d9e98bcb744
Abimael Ordoñez Avila <abimael.avila@mangausistemas.com>
2020-05-13 00:22:44 -0600

    Added Users and Cities relation.

---
+++ Mangau.WillNeedUmbrella.Infrastructure.SqLite/Migrations/20200513061448_UsersCities.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mangau.WillNeedUmbrella.Infrastructure.SqLite.Migrations
+{
+    public partial class UsersCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "secuser",
+                maxLength: 128,
+                nullable: true,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "wnuusercity",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false),
+                    CityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wnuusercity", x => new { x.UserId, x.CityId });
+                    table.ForeignKey(
+                        name: "FK_wnuusercity_wnucity_CityId",
+                        column: x => x.CityId,
+                        principalTable: "wnucity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_wnuusercity_secuser_UserId",
+                        column: x => x.UserId,
+                        principalTable: "secuser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_wnuusercity_CityId",
+                table: "wnuusercity",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "wnuusercity");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "secuser");
+        }
+    }
+}
---
+++ Mangau.WillNeedUmbrella/Entities/UserCity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mangau.WillNeedUmbrella.Entities
+{
+    [Table("wnuusercity")]
+    public class UserCity
+    {
+        public long UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int CityId { get; set; }
+
+        public City City { get; set; }
+    }
+}
